995666cb1a005b96e142d0ef22db8f60e4e1213b
Omkar Salunkhe <96873232+omkarsalunkheatp1211@users.noreply.github.com>
2024-03-26 12:40:45 +0530

    Update Projects.js

--- Projects.js
+++ Projects.js
@@ -48,12 +48,6 @@ view_shell_six.addEventListener("click", () => {
     window.open("https://github.com/omkarsalunkheatp1211/System_Monitoring_Shell_Scripts.git", "_blank");
 });
 
-//shell seven
-const view_shell_seven = document.getElementById("view_shell_seven");
-
-view_shell_seven.addEventListener("click", () => {
-    window.open("https://github.com/omkarsalunkheatp1211/Archive_And_Compresses_Files_Shell_Script_Project.git", "_blank");
-});
 
 // Ansible Scripting Project's
 //Ansible one
@@ -78,112 +72,36 @@ view_Ansible_third.addEventListener("click", () => {
 });
 
 //Web Development Project's
+
 //one
 const view_one = document.getElementById("view_one");
 
 title_one.addEventListener("click", () => {
-    window.open("https://omkarsalunkheatp1211.github.io/Battery_Status/", "_blank");
+    window.open("https://omkarsalunkheatp1211.github.io/Digital-Clock", "_blank");
 });
 
 view_one.addEventListener("click", () => {
-    window.open("https://github.com/omkarsalunkheatp1211/Battery_Status", "_blank");
+    window.open("https://github.com/omkarsalunkheatp1211/Digital-Clock", "_blank");
 });
 
 //two
 const view_two = document.getElementById("view_two");
 
 title_two.addEventListener("click", () => {
-    window.open("https://omkarsalunkheatp1211.github.io/Analog_clock/", "_blank");
+    window.open("https://omkarsalunkheatp1211.github.io/IP-Addresses/", "_blank");
 });
 
 view_two.addEventListener("click", () => {
-    window.open("https://github.com/omkarsalunkheatp1211/Analog_clock", "_blank");
+    window.open("https://github.com/omkarsalunkheatp1211/IP-Addresses", "_blank");
 });
 
 //three
 const view_three = document.getElementById("view_three");
 
 title_three.addEventListener("click", () => {
-    window.open("https://omkarsalunkheatp1211.github.io/Digital-Clock", "_blank");
-});
-
-view_three.addEventListener("click", () => {
-    window.open("https://github.com/omkarsalunkheatp1211/Digital-Clock", "_blank");
-});
-
-//four
-const view_four = document.getElementById("view_four");
-
-title_four.addEventListener("click", () => {
-    window.open("https://omkarsalunkheatp1211.github.io/Text-Encryption/", "_blank");
-});
-
-view_four.addEventListener("click", () => {
-    window.open("https://github.com/omkarsalunkheatp1211/Text-Encryption", "_blank");
-});
-
-//five
-// const view_five = document.getElementById("view_five");
-
-// title_five.addEventListener("click", () => {
-//     window.open("https://omkarsalunkheatp1211.github.io/Battery_Status/", "_blank");
-// });
-
-// view_five.addEventListener("click", () => {
-//     window.open("https://github.com/omkarsalunkheatp1211/Battery_Status", "_blank");
-// });
-
-//six
-const view_six = document.getElementById("view_six");
-
-title_six.addEventListener("click", () => {
-    window.open("https://omkarsalunkheatp1211.github.io/Text-Hashing-Tool/", "_blank");
-});
-
-view_six.addEventListener("click", () => {
-    window.open("https://github.com/omkarsalunkheatp1211/Text-Hashing-Tool", "_blank");
-});
-
-//seven
-const view_seven = document.getElementById("view_seven");
-
-title_seven.addEventListener("click", () => {
-    window.open("https://omkarsalunkheatp1211.github.io/IP-Addresses/", "_blank");
-});
-
-view_seven.addEventListener("click", () => {
-    window.open("https://github.com/omkarsalunkheatp1211/IP-Addresses", "_blank");
-});
-
-//eight
-const view_eight = document.getElementById("view_eight");
-
-title_eight.addEventListener("click", () => {
-    window.open("https://omkarsalunkheatp1211.github.io/Happy-Independence-Day/", "_blank");
-});
-
-view_eight.addEventListener("click", () => {
-    window.open("https://github.com/omkarsalunkheatp1211/Happy-Independence-Day", "_blank");
-});
-
-//nine
-const view_nine = document.getElementById("view_nine");
-
-title_nine.addEventListener("click", () => {
-    window.open("https://omkarsalunkheatp1211.github.io/transaction-website/", "_blank");
-});
-
-view_nine.addEventListener("click", () => {
-    window.open("https://github.com/omkarsalunkheatp1211/transaction-website", "_blank");
-});
-
-//ten
-const view_ten = document.getElementById("view_ten");
-
-title_ten.addEventListener("click", () => {
     window.open("https://github.com/omkarsalunkheatp1211/Text-Encryption-And-Decryption-in-java/", "_blank");
 });
 
-view_ten.addEventListener("click", () => {
+view_three.addEventListener("click", () => {
     window.open("https://github.com/omkarsalunkheatp1211/Text-Encryption-And-Decryption-in-java", "_blank");
 });
